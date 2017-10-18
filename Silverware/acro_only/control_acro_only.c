65af732c42339d22022202532837850e20aace21
silver13 <a@a.com>
2017-10-19 08:38:19 +1030

    add acro only build

---
+++ Silverware/acro_only/control_acro_only.c
@@ -0,0 +1,694 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 silverx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include <inttypes.h>
+#include <math.h>
+
+#include "pid.h"
+#include "config.h"
+#include "util.h"
+#include "drv_pwm.h"
+#include "control.h"
+#include "defines.h"
+#include "drv_time.h"
+#include "sixaxis.h"
+#include "drv_fmc2.h"
+#include "drv_fmc.h"
+#include "gestures.h"
+#include "defines.h"
+#include "led.h"
+
+
+extern float rx[];
+extern float gyro[3];
+extern int failsafe;
+extern float pidoutput[PIDNUMBER];
+
+extern float angleerror[];
+extern float attitude[];
+
+int onground = 1;
+int onground_long = 1;
+int pid_gestures_used = 0;
+
+float thrsum;
+
+float error[PIDNUMBER];
+float motormap( float input);
+
+float yawangle;
+
+extern float looptime;
+
+extern char auxchange[AUXNUMBER];
+extern char aux[AUXNUMBER];
+
+extern int ledcommand;
+extern int ledblink;
+
+extern float apid(int x);
+
+#ifdef NOMOTORS
+// to maintain timing or it will be optimized away
+float tempx[4];
+#endif
+
+#ifdef STOCK_TX_AUTOCENTER
+float autocenter[3];
+float lastrx[3];
+unsigned int consecutive[3];
+#endif
+
+unsigned long timecommand = 0;
+
+
+
+float overthrottlefilt = 0;
+float underthrottlefilt = 0;
+
+float rxcopy[4];
+
+void control( void)
+{	
+
+// rates / expert mode
+float rate_multiplier = 1.0;
+	
+	if ( aux[RATES]  )
+	{		
+		
+	}
+	else
+	{
+		rate_multiplier = LOW_RATES_MULTI;
+	}
+	// make local copy
+	
+
+	
+	
+	for ( int i = 0 ; i < 3 ; i++)
+	{
+		#ifdef STOCK_TX_AUTOCENTER
+		rxcopy[i] = (rx[i] - autocenter[i])* rate_multiplier;
+		#else
+		rxcopy[i] = rx[i] * rate_multiplier;
+		#endif
+	}
+
+
+
+	
+	// check for accelerometer calibration command
+	if ( onground )
+	{
+		#ifndef DISABLE_GESTURES2
+		int command = gestures2();
+
+		if (command!=GESTURE_NONE)
+        {
+            if (command == GESTURE_DDD)
+		    {
+			                  
+                //skip accel calibration if pid gestures used
+                if ( !pid_gestures_used )
+                {
+                    gyro_cal();	// for flashing lights
+                    acc_cal();                   
+                }
+                else
+                {
+                    ledcommand = 1;
+                    pid_gestures_used = 0;
+                }
+                #ifdef FLASH_SAVE2
+                extern float accelcal[3];
+                flash2_fmc_write( accelcal[0] + 127 , accelcal[1] + 127);
+                #endif
+                
+                #ifdef FLASH_SAVE1
+			    extern void flash_save( void);
+                extern void flash_load( void);
+                flash_save( );
+                flash_load( );
+                #endif
+			    // reset loop time 
+			    extern unsigned long lastlooptime;
+			    lastlooptime = gettime();
+		    }		
+
+            if (command == GESTURE_RRD)
+              {
+                  aux[CH_AUX1] = 1;
+                  ledcommand = 1;
+              }
+            if (command == GESTURE_LLD)
+              {
+                  ledcommand = 1;
+                  aux[CH_AUX1] = 0;
+              }
+            #ifdef PID_GESTURE_TUNING              
+            if ( command >= GESTURE_UDR ) pid_gestures_used = 1;   
+              
+           // int blink = 0;
+            if (command == GESTURE_UDU)
+              {
+                        // Cycle to next pid term (P I D)
+                        ledblink = next_pid_term();
+              }
+            if (command == GESTURE_UDD)
+              {
+                        // Cycle to next axis (Roll Pitch Yaw)
+                        ledblink = next_pid_axis();
+              }
+            if (command == GESTURE_UDR)
+              {
+                  // Increase by 10%
+                        ledblink = increase_pid();
+              }
+            if (command == GESTURE_UDL)
+              {
+                        // Descrease by 10%
+                  ledblink = decrease_pid();
+              }
+                // U D U - Next PID term
+                // U D D - Next PID Axis
+                // U D R - Increase value
+                // U D L - Descrease value
+               // ledblink = blink; //Will cause led logic to blink the number of times ledblink has stored in it.
+                #endif
+
+	  }
+		#endif		
+	}
+
+
+pid_precalc();	
+
+
+	// flight control
+	if (0)
+	  {	   // level mode
+           // level calculations done after to reduce latency in acro mode
+          
+	  }
+	else
+	  {	// rate mode
+
+		  error[0] = rxcopy[0] * (float) MAX_RATE * DEGTORAD  - gyro[0];
+		  error[1] = rxcopy[1] * (float) MAX_RATE * DEGTORAD  - gyro[1];
+
+          error[2] = rxcopy[2] * (float) MAX_RATEYAW * DEGTORAD  - gyro[2];
+
+	  }
+
+
+	pid(0);
+	pid(1);
+	pid(2);
+
+
+float	throttle;
+
+// map throttle so under 10% it is zero	
+if ( rx[3] < 0.1f ) throttle = 0;
+else throttle = (rx[3] - 0.1f)*1.11111111f;
+
+
+// turn motors off if throttle is off and pitch / roll sticks are centered
+	if ( failsafe || (throttle < 0.001f && (!ENABLESTIX || !onground_long || (fabsf(rx[ROLL]) < (float) ENABLESTIX_TRESHOLD && fabsf(rx[PITCH]) < (float) ENABLESTIX_TRESHOLD && fabsf(rx[YAW]) < (float) ENABLESTIX_TRESHOLD ) ) ) ) 
+	{	// motors off
+
+		if ( onground_long )
+		{
+			if ( gettime() - onground_long > ENABLESTIX_TIMEOUT)
+			{
+				onground_long = 0;
+			}
+		}	
+		
+		for ( int i = 0 ; i <= 3 ; i++)
+		{
+			pwm_set( i , 0 );	
+			#ifdef MOTOR_FILTER	
+			// reset the motor filter
+			motorfilter( 0 , i);
+			#endif
+		}	
+		
+		#ifdef MOTOR_BEEPS
+		extern void motorbeep( void);
+		motorbeep();
+		#endif
+
+		#ifdef MIX_LOWER_THROTTLE
+		// reset the overthrottle filter
+		lpf(&overthrottlefilt, 0.0f, 0.72f);	// 50hz 1khz sample rate
+		lpf(&underthrottlefilt, 0.0f, 0.72f);	// 50hz 1khz sample rate
+		#endif				
+		
+		#ifdef STOCK_TX_AUTOCENTER
+		for( int i = 0 ; i <3;i++)
+			{
+				if ( rx[i] == lastrx[i] )
+					{
+						consecutive[i]++;
+						
+					}
+				else consecutive[i] = 0;
+				lastrx[i] = rx[i];
+				if ( consecutive[i] > 1000 && fabsf( rx[i]) < 0.1f )
+					{
+						autocenter[i] = rx[i];
+					}
+			}
+		#endif				
+		
+		onground = 1;
+		thrsum = 0;
+		
+	}
+	else
+	{// motors on - normal flight
+		
+		onground = 0;
+		onground_long = gettime();
+		
+		float mix[4];	
+
+#ifdef 	THROTTLE_TRANSIENT_COMPENSATION
+        
+#ifndef THROTTLE_TRANSIENT_COMPENSATION_FACTOR 
+ #define THROTTLE_TRANSIENT_COMPENSATION_FACTOR 7.0 
+#endif        
+extern float throttlehpf( float in );
+        
+		  throttle += (float) (THROTTLE_TRANSIENT_COMPENSATION_FACTOR) * throttlehpf(throttle);
+		  if (throttle < 0)
+			  throttle = 0;
+		  if (throttle > 1.0f)
+			  throttle = 1.0f;
+#endif
+		
+		
+	
+		  // throttle angle compensation
+#ifdef AUTO_THROTTLE
+		  if (aux[LEVELMODE])
+		    {
+			    //float autothrottle = fastcos(attitude[0] * DEGTORAD) * fastcos(attitude[1] * DEGTORAD);
+			    extern float GEstG[];
+				float autothrottle = GEstG[2];
+				float old_throttle = throttle;
+			    if (autothrottle <= 0.5f)
+				    autothrottle = 0.5f;
+			    throttle = throttle / autothrottle;
+			    // limit to 90%
+			    if (old_throttle < 0.9f)
+				    if (throttle > 0.9f)
+					    throttle = 0.9f;
+
+			    if (throttle > 1.0f)
+				    throttle = 1.0f;
+
+		    }
+#endif
+	
+
+
+#ifdef LVC_LOWER_THROTTLE
+extern float vbatt_comp;
+extern float vbattfilt;
+
+static float throttle_i = 0.0f;
+
+ float throttle_p = 0.0f;
+
+// can be made into a function
+if (vbattfilt < (float) LVC_LOWER_THROTTLE_VOLTAGE_RAW ) 
+   throttle_p = ((float) LVC_LOWER_THROTTLE_VOLTAGE_RAW - vbattfilt) *(float) LVC_LOWER_THROTTLE_KP;
+// can be made into a function
+if (vbatt_comp < (float) LVC_LOWER_THROTTLE_VOLTAGE) 
+   throttle_p = ((float) LVC_LOWER_THROTTLE_VOLTAGE - vbatt_comp) *(float) LVC_LOWER_THROTTLE_KP;	
+
+if ( throttle_p > 1.0f ) throttle_p = 1.0f;
+
+if ( throttle_p > 0 ) 
+{
+    throttle_i += throttle_p * 0.0001f; //ki
+}
+else throttle_i -= 0.001f;// ki on release
+
+if ( throttle_i > 0.5f) throttle_i = 0.5f;
+if ( throttle_i < 0.0f) throttle_i = 0.0f;
+
+throttle -= throttle_p + throttle_i;
+#endif
+
+#ifdef INVERT_YAW_PID
+pidoutput[2] = -pidoutput[2];			
+#endif
+		
+		mix[MOTOR_FR] = throttle - pidoutput[ROLL] - pidoutput[PITCH] + pidoutput[YAW];		// FR
+		mix[MOTOR_FL] = throttle + pidoutput[ROLL] - pidoutput[PITCH] - pidoutput[YAW];		// FL	
+		mix[MOTOR_BR] = throttle - pidoutput[ROLL] + pidoutput[PITCH] - pidoutput[YAW];		// BR
+		mix[MOTOR_BL] = throttle + pidoutput[ROLL] + pidoutput[PITCH] + pidoutput[YAW];		// BL	
+		
+#ifdef INVERT_YAW_PID
+// we invert again cause it's used by the pid internally (for limit)
+pidoutput[2] = -pidoutput[2];			
+#endif
+
+		for ( int i = 0 ; i <= 3 ; i++)
+		{			
+		#ifdef MOTOR_FILTER		
+		mix[i] = motorfilter(  mix[i] , i);
+		#endif	
+		
+        #ifdef MOTOR_FILTER2_ALPHA	
+        float motorlpf( float in , int x) ;           
+		mix[i] = motorlpf(  mix[i] , i);
+		#endif	
+        }
+
+
+#if ( defined MIX_LOWER_THROTTLE || defined MIX_INCREASE_THROTTLE)
+
+//#define MIX_INCREASE_THROTTLE
+
+// options for mix throttle lowering if enabled
+// 0 - 100 range ( 100 = full reduction / 0 = no reduction )
+#ifndef MIX_THROTTLE_REDUCTION_PERCENT
+#define MIX_THROTTLE_REDUCTION_PERCENT 100
+#endif
+// lpf (exponential) shape if on, othewise linear
+//#define MIX_THROTTLE_FILTER_LPF
+
+// limit reduction and increase to this amount ( 0.0 - 1.0)
+// 0.0 = no action 
+// 0.5 = reduce up to 1/2 throttle      
+//1.0 = reduce all the way to zero 
+#ifndef MIX_THROTTLE_REDUCTION_MAX
+#define MIX_THROTTLE_REDUCTION_MAX 0.5
+#endif
+
+#ifndef MIX_MOTOR_MAX
+#define MIX_MOTOR_MAX 1.0f
+#endif
+
+
+		  float overthrottle = 0;
+		  float underthrottle = 0.001f;
+		
+		  for (int i = 0; i < 4; i++)
+		    {
+			    if (mix[i] > overthrottle)
+				    overthrottle = mix[i];
+					if (mix[i] < underthrottle)
+						underthrottle = mix[i];
+		    }
+
+#ifdef MIX_LOWER_THROTTLE
+            
+		  overthrottle -= MIX_MOTOR_MAX ;
+
+		  if (overthrottle > (float)MIX_THROTTLE_REDUCTION_MAX)
+			  overthrottle = (float)MIX_THROTTLE_REDUCTION_MAX;
+
+#ifdef MIX_THROTTLE_FILTER_LPF
+		  if (overthrottle > overthrottlefilt)
+			  lpf(&overthrottlefilt, overthrottle, 0.82);	// 20hz 1khz sample rate
+		  else
+			  lpf(&overthrottlefilt, overthrottle, 0.72);	// 50hz 1khz sample rate
+#else
+		  if (overthrottle > overthrottlefilt)
+			  overthrottlefilt += 0.005f;
+		  else
+			  overthrottlefilt -= 0.01f;
+#endif
+#else
+overthrottle = 0.0f;        
+#endif
+          
+#ifdef MIX_INCREASE_THROTTLE
+// under			
+			
+		  if (underthrottle < -(float)MIX_THROTTLE_REDUCTION_MAX)
+			  underthrottle = -(float)MIX_THROTTLE_REDUCTION_MAX;
+			
+#ifdef MIX_THROTTLE_FILTER_LPF
+		  if (underthrottle < underthrottlefilt)
+			  lpf(&underthrottlefilt, underthrottle, 0.82);	// 20hz 1khz sample rate
+		  else
+			  lpf(&underthrottlefilt, underthrottle, 0.72);	// 50hz 1khz sample rate
+#else
+		  if (underthrottle < underthrottlefilt)
+			  underthrottlefilt -= 0.005f;
+		  else
+			  underthrottlefilt += 0.01f;
+#endif
+// under
+			if (underthrottlefilt < - (float)MIX_THROTTLE_REDUCTION_MAX)
+			  underthrottlefilt = - (float)MIX_THROTTLE_REDUCTION_MAX;
+		  if (underthrottlefilt > 0.1f)
+			  underthrottlefilt = 0.1;
+
+			underthrottle = underthrottlefilt;
+					
+			if (underthrottle > 0.0f)
+			  underthrottle = 0.0001f;
+
+			underthrottle *= ((float)MIX_THROTTLE_REDUCTION_PERCENT / 100.0f);
+#else
+  underthrottle = 0.001f;			
+#endif			
+// over			
+		  if (overthrottlefilt > (float)MIX_THROTTLE_REDUCTION_MAX)
+			  overthrottlefilt = (float)MIX_THROTTLE_REDUCTION_MAX;
+		  if (overthrottlefilt < -0.1f)
+			  overthrottlefilt = -0.1;
+
+
+		  overthrottle = overthrottlefilt;
+
+			
+		  if (overthrottle < 0.0f)
+			  overthrottle = -0.0001f;
+
+			
+			// reduce by a percentage only, so we get an inbetween performance
+			overthrottle *= ((float)MIX_THROTTLE_REDUCTION_PERCENT / 100.0f);
+
+			
+			
+		  if (overthrottle > 0 || underthrottle < 0 )
+		    {		// exceeding max motor thrust
+					float temp = overthrottle + underthrottle;
+			    for (int i = 0; i < 4; i++)
+			      {
+				      mix[i] -= temp;
+			      }
+		    }
+#endif				
+
+
+#ifdef MIX_LOWER_THROTTLE_3
+{
+#ifndef MIX_THROTTLE_REDUCTION_MAX
+#define MIX_THROTTLE_REDUCTION_MAX 0.5f
+#endif
+
+float overthrottle = 0;
+
+for (int i = 0; i < 4; i++)
+		    {
+			    if (mix[i] > overthrottle)
+				    overthrottle = mix[i];
+            }
+
+
+overthrottle -=1.0f;
+// limit to half throttle max reduction
+if ( overthrottle > (float) MIX_THROTTLE_REDUCTION_MAX)  overthrottle = (float) MIX_THROTTLE_REDUCTION_MAX;
+
+if ( overthrottle > 0.0f)
+{
+    for ( int i = 0 ; i < 4 ; i++)
+        mix[i] -= overthrottle;
+}
+#ifdef MIX_THROTTLE_FLASHLED
+if ( overthrottle > 0.1f) ledcommand = 1;
+#endif
+}
+#endif
+
+
+#ifdef MIX_INCREASE_THROTTLE_3
+{
+#ifndef MIX_THROTTLE_INCREASE_MAX
+#define MIX_THROTTLE_INCREASE_MAX 0.2f
+#endif
+
+float underthrottle = 0;
+
+for (int i = 0; i < 4; i++)
+    {
+        if (mix[i] < underthrottle)
+            underthrottle = mix[i];
+    }
+
+
+// limit to half throttle max reduction
+if ( underthrottle < -(float) MIX_THROTTLE_INCREASE_MAX)  underthrottle = -(float) MIX_THROTTLE_INCREASE_MAX;
+
+if ( underthrottle < 0.0f)
+    {
+        for ( int i = 0 ; i < 4 ; i++)
+            mix[i] -= underthrottle;
+    }
+#ifdef MIX_THROTTLE_FLASHLED
+if ( underthrottle < -0.01f) ledcommand = 1;
+#endif
+}
+#endif
+
+            
+            
+            
+thrsum = 0;		
+				
+		for ( int i = 0 ; i <= 3 ; i++)
+		{			
+		           
+		#ifdef CLIP_FF
+		mix[i] = clip_ff(mix[i], i);
+		#endif
+
+		#ifdef MOTORS_TO_THROTTLE
+		mix[i] = throttle;
+		// flash leds in valid throttle range
+		ledcommand = 1;
+		#warning "MOTORS TEST MODE"
+		#endif
+
+		#ifdef MOTOR_MIN_ENABLE
+		if (mix[i] < (float) MOTOR_MIN_VALUE)
+		{
+			mix[i] = (float) MOTOR_MIN_VALUE;
+		}
+		#endif
+		
+			
+		#ifndef NOMOTORS
+		#ifndef MOTORS_TO_THROTTLE
+		//normal mode
+		pwm_set( i ,motormap( mix[i] ) );
+		#else
+		// throttle test mode
+		ledcommand = 1;
+		pwm_set( i , mix[i] );
+		#endif
+		#else
+		// no motors mode ( anti-optimization)
+		#warning "NO MOTORS"
+		tempx[i] = motormap( mix[i] );
+		#endif
+		
+		if ( mix[i] < 0 ) mix[i] = 0;
+		if ( mix[i] > 1 ) mix[i] = 1;
+		thrsum+= mix[i];
+		}	
+		thrsum = thrsum / 4;
+		
+	}// end motors on
+
+    
+	
+}
+
+
+#ifndef MOTOR_FILTER2_ALPHA
+#define MOTOR_FILTER2_ALPHA 0.3
+#endif
+
+
+float motor_filt[4];
+
+float motorlpf( float in , int x)
+{
+    
+    lpf(&motor_filt[x] , in , 1 - MOTOR_FILTER2_ALPHA);
+       
+    return motor_filt[x];
+}
+
+
+float hann_lastsample[4];
+float hann_lastsample2[4];
+
+// hanning 3 sample filter
+float motorfilter( float motorin ,int number)
+{
+ 	float ans = motorin*0.25f + hann_lastsample[number] * 0.5f +   hann_lastsample2[number] * 0.25f ;
+	
+	hann_lastsample2[number] = hann_lastsample[number];
+	hann_lastsample[number] = motorin;
+	
+	return ans;
+}
+
+
+float clip_feedforward[4];
+// clip feedforward adds the amount of thrust exceeding 1.0 ( max) 
+// to the next iteration(s) of the loop
+// so samples 0.5 , 1.5 , 0.4 would transform into 0.5 , 1.0 , 0.9;
+
+float clip_ff(float motorin, int number)
+{
+
+	if (motorin > 1.0f)
+	  {
+		  clip_feedforward[number] += (motorin - 1.0f);
+		  //cap feedforward to prevent windup 
+		  if (clip_feedforward[number] > .5f)
+			  clip_feedforward[number] = .5f;
+	  }
+	else if (clip_feedforward[number] > 0)
+	  {
+		  float difference = 1.0f - motorin;
+		  motorin = motorin + clip_feedforward[number];
+		  if (motorin > 1.0f)
+		    {
+			    clip_feedforward[number] -= difference;
+			    if (clip_feedforward[number] < 0)
+				    clip_feedforward[number] = 0;
+		    }
+		  else
+			  clip_feedforward[number] = 0;
+
+	  }
+	return motorin;
+}
+
+
---
+++ Silverware/acro_only/main_acro_only.c
@@ -0,0 +1,525 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016 silverx
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+// STM32 acro firmware
+// files of this project should be assumed MIT licence unless otherwise noted
+
+
+#include "project.h"
+
+#include "led.h"
+#include "util.h"
+#include "sixaxis.h"
+#include "drv_adc.h"
+#include "drv_time.h"
+#include "drv_softi2c.h"
+#include "config.h"
+#include "drv_pwm.h"
+#include "drv_adc.h"
+#include "drv_gpio.h"
+#include "drv_serial.h"
+#include "rx.h"
+#include "drv_spi.h"
+#include "control.h"
+#include "pid.h"
+#include "defines.h"
+#include "drv_i2c.h"
+#include "drv_softi2c.h"
+#include "drv_serial.h"
+#include "buzzer.h"
+#include "drv_fmc2.h"
+
+#include "binary.h"
+
+#include <stdio.h>
+#include <math.h>
+#include <inttypes.h>
+
+
+
+#ifdef __GNUC__
+#ifndef SOFT_LPF_NONE
+#warning the soft lpf may not work correctly with gcc due to longer loop time
+#endif
+#endif
+
+
+#ifdef DEBUG
+#include "debug.h"
+debug_type debug;
+#endif
+
+
+
+
+
+// hal
+void clk_init(void);
+extern void flash_load( void);
+extern void flash_hard_coded_pid_identifier(void);
+
+
+// looptime in seconds
+float looptime;
+// filtered battery in volts
+float vbattfilt = 0.0;
+float vbatt_comp = 4.2;
+// voltage reference for vcc compensation
+float vreffilt = 1.0;
+// average of all motors
+float thrfilt = 0;
+
+unsigned int lastlooptime;
+// signal for lowbattery
+int lowbatt = 1;	
+
+//int minindex = 0;
+
+// holds the main four channels, roll, pitch , yaw , throttle
+float rx[4];
+
+// holds auxilliary channels
+// the last 2 are always on and off respectively
+char aux[AUXNUMBER] = { 0 ,0 ,0 , 0 , 0 , 0};
+char lastaux[AUXNUMBER];
+// if an aux channel has just changed
+char auxchange[AUXNUMBER];
+
+// bind / normal rx mode
+extern int rxmode;
+// failsafe on / off
+extern int failsafe;
+extern float hardcoded_pid_identifier;
+
+// for led flash on gestures
+int ledcommand = 0;
+int ledblink = 0;
+unsigned long ledcommandtime = 0;
+
+void failloop( int val);
+
+int random_seed = 0;
+
+int main(void)
+{
+	
+	delay(1000);
+
+
+#ifdef ENABLE_OVERCLOCK
+clk_init();
+#endif
+	
+  gpio_init();	
+	
+	spi_init();
+	
+  time_init();
+
+	delay(100000);
+		
+	i2c_init();	
+	
+	pwm_init();
+
+	pwm_set( MOTOR_BL , 0);
+	pwm_set( MOTOR_FL , 0);	 
+	pwm_set( MOTOR_FR , 0); 
+	pwm_set( MOTOR_BR , 0); 
+
+
+	sixaxis_init();
+	
+	if ( sixaxis_check() ) 
+	{
+		
+	}
+	else 
+	{
+        //gyro not found   
+		failloop(4);
+	}
+	
+	adc_init();
+//set always on channel to on
+aux[CH_ON] = 1;	
+	
+#ifdef AUX1_START_ON
+aux[CH_AUX1] = 1;
+#endif
+	rx_init();
+
+	
+int count = 0;
+	
+while ( count < 64 )
+{
+	vbattfilt += adc_read(0);
+	delay(1000);
+	count++;
+}
+#ifdef RX_BAYANG_BLE_APP
+   // for randomising MAC adddress of ble app - this will make the int = raw float value        
+    random_seed =  *(int *)&vbattfilt ; 
+    random_seed = random_seed&0xff;
+#endif
+ vbattfilt = vbattfilt/64;	
+// startvref = startvref/64;
+
+	
+#ifdef STOP_LOWBATTERY
+// infinite loop
+if ( vbattfilt < (float) 3.3f) failloop(2);
+#endif
+
+
+
+gyro_cal();
+
+extern void rgb_init( void);
+rgb_init();
+
+#ifdef SERIAL_ENABLE
+serial_init();
+#endif
+
+
+
+
+#ifdef FLASH_SAVE2
+// read accelerometer calibration values from option bytes ( 2* 8bit)
+extern float accelcal[3];
+ accelcal[0] = flash2_readdata( OB->DATA0 ) - 127;
+ accelcal[1] = flash2_readdata( OB->DATA1 ) - 127;
+#endif
+
+#ifdef FLASH_SAVE1
+// read pid identifier for values in file pid.c
+    flash_hard_coded_pid_identifier();
+
+// load flash saved variables
+    flash_load( );
+#endif
+
+extern int liberror;
+if ( liberror ) 
+{
+		failloop(7);
+}
+
+
+
+ lastlooptime = gettime();
+
+
+//
+//
+// 		MAIN LOOP
+//
+//
+
+
+	while(1)
+	{
+		// gettime() needs to be called at least once per second 
+		unsigned long time = gettime(); 
+		looptime = ((uint32_t)( time - lastlooptime));
+		if ( looptime <= 0 ) looptime = 1;
+		looptime = looptime * 1e-6f;
+		if ( looptime > 0.02f ) // max loop 20ms
+		{
+			failloop( 6);	
+			//endless loop			
+		}
+	
+		#ifdef DEBUG				
+		debug.totaltime += looptime;
+		lpf ( &debug.timefilt , looptime, 0.998 );
+		#endif
+		lastlooptime = time;
+		
+		if ( liberror > 20) 
+		{
+			failloop(8);
+			// endless loop
+		}
+
+        // read gyro and accelerometer data	
+		gyro_read();
+		
+		
+        // all flight calculations and motors
+		control();
+      
+      
+// battery low logic
+
+#ifdef ADC_VREF_SCALE
+        // account for vcc changes
+		float battadc = adc_read(0)*vreffilt; 
+        lpf ( &vreffilt , adc_read(1)  , 0.9968f);	
+#else
+        float battadc = adc_read(0); 
+#endif        
+		
+
+		// average of all 4 motor thrusts
+		// should be proportional with battery current			
+		extern float thrsum; // from control.c
+	
+		// filter motorpwm so it has the same delay as the filtered voltage
+		// ( or they can use a single filter)		
+		lpf ( &thrfilt , thrsum , 0.9968f);	// 0.5 sec at 1.6ms loop time	
+
+        static float vbattfilt_corr = 4.2;
+        // li-ion battery model compensation time decay ( 18 seconds )
+        lpf ( &vbattfilt_corr , vbattfilt , FILTERCALC( 1000 , 18000e3) );
+	
+        lpf ( &vbattfilt , battadc , 0.9968f);
+
+
+// compensation factor for li-ion internal model
+// zero to bypass
+#define CF1 0.25f
+
+        float tempvolt = vbattfilt*( 1.00f + CF1 )  - vbattfilt_corr* ( CF1 );
+
+#ifdef AUTO_VDROP_FACTOR
+
+static float lastout[12];
+static float lastin[12];
+static float vcomp[12];
+static float score[12];
+static int z = 0;
+static int minindex = 0;
+static int firstrun = 1;
+
+
+if( thrfilt > 0.1f )
+{
+	vcomp[z] = tempvolt + (float) z *0.1f * thrfilt;
+		
+	if ( firstrun ) 
+    {
+        for (int y = 0 ; y < 12; y++) lastin[y] = vcomp[z];
+        firstrun = 0;
+    }
+	float ans;
+	//	y(n) = x(n) - x(n-1) + R * y(n-1) 
+	//  out = in - lastin + coeff*lastout
+		// hpf
+	ans = vcomp[z] - lastin[z] + FILTERCALC( 1000*12 , 6000e3) *lastout[z];
+	lastin[z] = vcomp[z];
+	lastout[z] = ans;
+	lpf ( &score[z] , ans*ans , FILTERCALC( 1000*12 , 60e6 ) );	
+	z++;
+       
+    if ( z >= 12 )
+    {
+        z = 0;
+        float min = score[0]; 
+        for ( int i = 0 ; i < 12; i++ )
+        {
+         if ( (score[i]) < min )  
+            {
+                min = (score[i]);
+                minindex = i;
+                // add an offset because it seems to be usually early
+                minindex++;
+            }
+        }   
+    }
+
+}
+
+#undef VDROP_FACTOR
+#define VDROP_FACTOR  minindex * 0.1f
+#endif
+
+    float hyst;
+    if ( lowbatt ) hyst = HYST;
+    else hyst = 0.0f;
+
+    if (( tempvolt + (float) VDROP_FACTOR * thrfilt <(float) VBATTLOW + hyst )
+        || ( vbattfilt < ( float ) 2.7f ) )
+        lowbatt = 1;
+    else lowbatt = 0;
+
+    vbatt_comp = tempvolt + (float) VDROP_FACTOR * thrfilt; 	
+
+           
+#ifdef DEBUG
+	debug.vbatt_comp = vbatt_comp ;
+#endif		
+	
+
+#if ( LED_NUMBER > 0)
+// led flash logic	
+if ( lowbatt )
+	ledflash ( 500000 , 8);
+else
+{
+		if ( rxmode == RXMODE_BIND)
+		{// bind mode
+		ledflash ( 100000, 12);
+		}else
+		{// non bind
+			if ( failsafe) 
+				{
+					ledflash ( 500000, 15);			
+				}
+			else 
+			{
+			
+				if (ledcommand)
+						  {
+							  if (!ledcommandtime)
+								  ledcommandtime = gettime();
+							  if (gettime() - ledcommandtime > 500000)
+							    {
+								    ledcommand = 0;
+								    ledcommandtime = 0;
+							    }
+							  ledflash(100000, 8);
+						  }
+               	#ifndef DISABLE_GESTURES2
+						else if (ledblink)
+						{
+							if (!ledcommandtime)
+								  ledcommandtime = gettime();
+							if (gettime() - ledcommandtime > 500000)
+							    {
+								    ledblink--;
+								    ledcommandtime = 0;
+							    }
+							ledflash(500000, 3);
+						}
+						else
+					#endif // end gesture led flash
+				if ( aux[LEDS_ON] )
+				#if( LED_BRIGHTNESS != 15)	
+				led_pwm(LED_BRIGHTNESS);
+				#else
+				ledon( 255);
+				#endif
+				else 
+				ledoff( 255);
+			}
+		} 		
+		
+	}
+#endif
+
+
+#if ( RGB_LED_NUMBER > 0)
+// RGB led control
+extern	void rgb_led_lvc( void);
+rgb_led_lvc( );
+#endif
+
+
+#ifdef BUZZER_ENABLE	
+	buzzer();
+#endif
+
+            
+#ifdef FPV_ON
+// fpv switch
+    static int fpv_init = 0;
+    if ( !fpv_init && rxmode == RXMODE_NORMAL ) {
+        fpv_init = gpio_init_fpv();
+        }
+    if ( fpv_init ) {
+        if ( failsafe ) {
+            GPIO_WriteBit( FPV_PORT, FPV_PIN, Bit_RESET );
+        } else {
+            GPIO_WriteBit( FPV_PORT, FPV_PIN, aux[ FPV_ON ] ? Bit_SET : Bit_RESET );
+        }
+    }
+#endif
+
+// receiver function
+checkrx();
+
+
+while ( (gettime() - time) < LOOPTIME );	
+
+		
+	}// end loop
+	
+
+}
+
+// 2 - low battery at powerup - if enabled by config
+
+// 4 - Gyro not found
+// 5 - clock , intterrupts , systick
+// 7 - i2c error 
+// 8 - i2c error main loop
+// 6 - loop time issue
+
+void failloop( int val)
+{
+	for ( int i = 0 ; i <= 3 ; i++)
+	{
+		pwm_set( i ,0 );
+	}	
+
+	while(1)
+	{
+		for ( int i = 0 ; i < val; i++)
+		{
+		 ledon( 255);		
+		 delay(200000);
+		 ledoff( 255);	
+		 delay(200000);			
+		}
+		delay(800000);
+	}	
+	
+}
+
+
+void HardFault_Handler(void)
+{
+	failloop(5);
+}
+void MemManage_Handler(void) 
+{
+	failloop(5);
+}
+void BusFault_Handler(void) 
+{
+	failloop(5);
+}
+void UsageFault_Handler(void) 
+{
+	failloop(5);
+}
+
+
+
+
+
